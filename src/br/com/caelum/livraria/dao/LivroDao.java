aa8461049231c5b191489ab9f1fd35e598011271
Aldecir Neves de Lima <aldecirlima@gmail.com>
2022-05-26 21:37:47 -0300

    Aula 7 primeiro vídeo concluído

--- src/br/com/caelum/livraria/dao/LivroDao.java
+++ src/br/com/caelum/livraria/dao/LivroDao.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 import br.com.caelum.livraria.modelo.Livro;
 
@@ -13,6 +14,7 @@ public class LivroDao {
 
 	@PersistenceContext
 	private EntityManager manager;
+	private TypedQuery<Livro> query;
 	
 	public void salva(Livro livro) {
 		manager.persist(livro);
@@ -20,6 +22,15 @@ public class LivroDao {
 	
 	public List<Livro> todosLivros() {
 		return manager.createQuery("SELECT l FROM Livro l", Livro.class).getResultList();
+	}
+
+	public List<Livro> livrosPeloNome(String nome) {
+		
+		query = this.manager.createQuery("SELECT l FROM Livro l where l.titulo like :pTitulo", Livro.class);
+		
+		query.setParameter("pTitulo", "%" + nome + "%");
+		
+		return query.getResultList();
 	}
 	
 }
